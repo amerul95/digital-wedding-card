81aad75e888b2d6a9088325b561b8ad08f5b1c9b
Polygot <mirolesuperman@gmail.com>
2025-10-13 00:42:23 +0800

    update components

---
+++ app/catalog/designs/page.tsx
@@ -0,0 +1,14 @@
+import React from 'react'
+
+export default function page() {
+  return (
+    <div>
+      <div>
+        search filter
+      </div>
+      <div>
+        items
+      </div>
+    </div>
+  )
+}
