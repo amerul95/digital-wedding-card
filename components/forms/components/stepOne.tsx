a11eb4688e621f238ae8ad06c7ba924b7f0384df
Polygot <mirolesuperman@gmail.com>
2025-10-31 00:29:04 +0800

    update

--- components/forms/components/stepOne.tsx
+++ components/forms/components/stepOne.tsx
@@ -100,7 +100,7 @@ export default function StepOne() {
                     id="form-rhf-demo-title"
                     aria-invalid={fieldState.invalid}
                     placeholder="Please insert Bride Name"
-                    autoComplete="of"
+                    autoComplete="off"
                   />
                 </Field>
               )}
